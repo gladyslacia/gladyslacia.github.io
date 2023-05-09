73b4a9b7d7be86d5c719099074bfd121e7a04968
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-05-10 03:39:17 +0800

    Add files via upload

---
+++ codes/function.js
@@ -0,0 +1,32 @@
+/** for navigation */
+function home() {
+    location.href="home.html";
+}
+function subjects() {
+    location.href="subjects.html";
+}
+function inquiry() {
+    location.href="inquiry.html";
+}
+function login() {
+    location.href="login.html";
+}
+function signup() {
+    location.href="signup.html";
+}
+/** for home page */
+function messenger() {
+    window.open("https://m.me/gladyslacia.rf.gd/");
+}
+function googlemail() {
+    window.open("mailto:emaildivert.gladyslacia@gmail.com");
+}
+/** for log-in user */
+function loginuser() {
+    location.href="home.html";
+}
+/** for sign-up user */
+function signupuser() {
+    location.href="subjects.html";
+}
+/** for user pages */
