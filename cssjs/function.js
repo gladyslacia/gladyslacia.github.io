95b8b225add1b3ace787080d15d970bde7408762
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-07-25 04:34:27 +0800

    Add files via upload

---
+++ cssjs/function.js
@@ -0,0 +1,18 @@
+/** for navigation */
+function home() {
+    location.href="index.html";
+}
+function classes() {
+    location.href="googleclassroom.html";
+}
+function lectures() {
+    location.href="lecturenotes.html";
+}
+function reviews() {
+    location.href="reviewers.html";
+}
+
+/** for buttons */
+function messenger() {
+    window.open("https://m.me/gl.gladyslacia/");
+}
