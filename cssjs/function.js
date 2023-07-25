8c03072d188cd3fd1e5e4c56b552aa33a2efb238
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-07-26 06:51:58 +0800

    Add files via upload

--- cssjs/function.js
+++ cssjs/function.js
@@ -24,15 +24,22 @@ function inquirymail1() {
     emailsubject4gl1.type = 'hidden';
     emailsubject4gl1.name = '_subject';
     emailsubject4gl1.value = 'Inquiry: From ' + emailaddress1;
-    document.querySelector('form').appendChild(emailsubject4gl1);
+
+    // Select the first form with class name "inquirymail1"
+    const form1 = document.querySelector('.inquirymail1 form');
+    form1.appendChild(emailsubject4gl1);
 }
+
 function inquirymail2() {
     const emailaddress2 = document.getElementById('emailaddressid2').value;
     const emailsubject4gl2 = document.createElement('input');
     emailsubject4gl2.type = 'hidden';
     emailsubject4gl2.name = '_subject';
     emailsubject4gl2.value = 'Inquiry: From ' + emailaddress2;
-    document.querySelector('form').appendChild(emailsubject4gl2);
+
+    // Select the second form with class name "inquirymail2"
+    const form2 = document.querySelector('.inquirymail2 form');
+    form2.appendChild(emailsubject4gl2);
 }
 
 /** soon to update */
