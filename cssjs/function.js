9015804290d4b630080f6de3ee2c927c51440c67
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-07-26 05:50:27 +0800

    Add files via upload

--- cssjs/function.js
+++ cssjs/function.js
@@ -18,4 +18,13 @@ function messenger() {
 }
 
 /** for email */
+function inquirymail() {
+    const emailaddress = document.getElementById('emailaddressid').value;
+    const emailsubject4gl = document.createElement('input');
+    emailsubject4gl.type = 'hidden';
+    emailsubject4gl.name = '_subject';
+    emailsubject4gl.value = 'Inquiry: From ' + emailaddress;
+    document.querySelector('form').appendChild(emailsubject4gl);
+}
+
 /** soon to update */
