f63c401cfe41ad4b12935ed27a8707a95d02b6e4
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-07-26 06:20:12 +0800

    Add files via upload

--- cssjs/function.js
+++ cssjs/function.js
@@ -18,13 +18,21 @@ function messenger() {
 }
 
 /** for email */
-function inquirymail() {
-    const emailaddress = document.getElementById('emailaddressid').value;
-    const emailsubject4gl = document.createElement('input');
-    emailsubject4gl.type = 'hidden';
-    emailsubject4gl.name = '_subject';
-    emailsubject4gl.value = 'Inquiry: From ' + emailaddress;
-    document.querySelector('form').appendChild(emailsubject4gl);
+function inquirymail1() {
+    const emailaddress1 = document.getElementById('emailaddressid1').value;
+    const emailsubject4gl1 = document.createElement('input');
+    emailsubject4gl1.type = 'hidden';
+    emailsubject4gl1.name = '_subject';
+    emailsubject4gl1.value = 'Inquiry: From ' + emailaddress1;
+    document.querySelector('form').appendChild(emailsubject4gl1);
+}
+function inquirymail2() {
+    const emailaddress2 = document.getElementById('emailaddressid2').value;
+    const emailsubject4gl2 = document.createElement('input');
+    emailsubject4gl2.type = 'hidden';
+    emailsubject4gl2.name = '_subject';
+    emailsubject4gl2.value = 'Inquiry: From ' + emailaddress2;
+    document.querySelector('form').appendChild(emailsubject4gl2);
 }
 
 /** soon to update */
