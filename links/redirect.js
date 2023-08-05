07f5b1d9a4ece83f396ed034b0127b8208f095a1
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-08-05 20:31:27 +0800

    Delete links directory

--- links/redirect.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var img = new Image();
-  img.onerror = function () {
-    // Redirect if the 'economics' file is not found
-    window.location.replace('https://www.google.com');
-  };
-  img.src = '/economics';
-})();
