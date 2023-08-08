403bc24149d7b0db71920875edc8c8b4d64360e1
GLADYS LACIA <133050838+gladyslacia@users.noreply.github.com>
2023-08-09 00:46:24 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-function inquirymail1() {
-  const emailaddress1 = document.getElementById('emailaddressid1').value;
-  const emailsubject4gl1 = document.createElement('input');
-  emailsubject4gl1.type = 'hidden'; emailsubject4gl1.name = '_subject';
-  emailsubject4gl1.value = 'Inquiry: From ' + emailaddress1;
-
-  // Select the first form with class name "inquirymail1"
-  const form1 = document.querySelector('.inquirymail1 form'); form1.appendChild(emailsubject4gl1);
-}
